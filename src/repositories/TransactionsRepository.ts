d5ea87c0611b8ae6a0d248794f80de72b5c22324
Ronaldo Silva <ronaldo.intermec@gmail.com>
2022-12-15 19:54:17 -0300

    challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,67 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const {income, outcome,} = this.transactions.reduce((accumulator: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          accumulator.income += transaction.value;
+          break;
+        case 'outcome':
+          accumulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return accumulator;
+
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
+
+    const total = income - outcome;
+
+    return {income, outcome, total};
+
+    // const incomeTransacitons = this.transactions.filter(transaction => (transaction.type.includes('income')));
+    // const outcomeTransations = this.transactions.filter(transaction => transaction.type.includes('outcome'));
+
+    // const incomeList = incomeTransacitons.map((income) => income.value);
+    // const outcomeList = outcomeTransations.map((income) => income.value);
+
+    // const initialincome = 0;
+    // const sumIncome = incomeList.reduce(
+    //   (accumulator, currentValue) => accumulator + currentValue,
+    //   initialincome
+    // );
+
+    // const initialOutcome = 0;
+    // const sumOutcome = outcomeList.reduce(
+    //   (accumulator, currentValue) => accumulator + currentValue,
+    //   initialOutcome
+    // );
+
+    // const amounnt = sumIncome - sumOutcome;
+    //    const balance: Balance = {
+    //   income: sumIncome,
+    //   outcome: sumOutcome,
+    //   total: sumIncome - amounnt,
+    // }
+
+    // return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,25 +1,35 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance() 
+    
+    return response.status(200).json({ transactions, balance});
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: err instanceof Error ? err.message : 'err não é uma intância da classe Error' });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
+
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransactionService = new CreateTransactionService(transactionsRepository);
+    const transaction = createTransactionService.execute({ title, value, type });
+
+    return response.status(200).json(transaction);
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: err instanceof Error ? err.message : "err não é uma instância da classe Error" });
   }
 });
 
